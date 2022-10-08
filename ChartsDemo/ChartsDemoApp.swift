03c7c6a3b362d0475397605b0784503221e029ef
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-10-08 06:27:30 -0500

    Initial Commit

---
+++ ChartsDemo/ChartsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsDemoApp.swift
+//  ChartsDemo
+//
+//  Created by Mark Volkmann on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ChartsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
