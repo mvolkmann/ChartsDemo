371fcae1ed9ce13b660f9d97a0612ea90c154c8d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-10-08 16:24:27 -0500

    improved a comment

--- ChartsDemo/Views/ContentView.swift
+++ ChartsDemo/Views/ContentView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     // TODO: Try getting data from the Census API at
-    // TODO: https://www.census.gov/programs-surveys/sbo/technical-documentation/api.html
+    // TODO: https://www.census.gov/data/developers/data-sets/census-microdata-api.html
     // TODO: Also test use of the Instruments tool to example HTTP traffic.
     var body: some View {
         TabView {
