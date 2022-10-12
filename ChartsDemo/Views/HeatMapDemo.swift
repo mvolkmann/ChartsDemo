f77dfbed851285f0df5898f5c8fff50d041e51d6
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-10-11 20:54:51 -0500

    progress on heat map

--- ChartsDemo/Views/HeatMapDemo.swift
+++ ChartsDemo/Views/HeatMapDemo.swift
@@ -1,11 +1,67 @@
+import Charts
 import SwiftUI
 
 struct HeatMapDemo: View {
+    // MARK: - State
+
+    @Environment(\.colorScheme) var colorScheme
+
+    // MARK: - Properties
+
+    private static let gradientColors: [Color] =
+        [.blue, .green, .yellow, .orange, .red]
+
+    private let squareSize = 10
+
+    private let vm = ViewModel.shared
+
     var body: some View {
-        VStack {
-            Text("Heat Map")
+        Chart {
+            ForEach(vm.statistics.indices, id: \.self) { index in
+                let xVal = 0
+                let yVal = index * squareSize
+                let statistic = vm.statistics[index]
+                let category = PlottableValue.value(
+                    "Age",
+                    statistic.category
+                )
+
+                Plot {
+                    RectangleMark(
+                        xStart: PlottableValue.value("xStart", xVal),
+                        xEnd: PlottableValue.value("xEnd", xVal + squareSize),
+                        yStart: PlottableValue.value("yStart", yVal),
+                        yEnd: PlottableValue.value("yEnd", yVal + squareSize)
+                    )
+                    .foregroundStyle(by: .value("Count", statistic.male))
+                }
+                Plot {
+                    RectangleMark(
+                        xStart: PlottableValue.value(
+                            "xStart",
+                            xVal + squareSize
+                        ),
+                        xEnd: PlottableValue.value(
+                            "xEnd",
+                            xVal + 2 * squareSize
+                        ),
+                        yStart: PlottableValue.value("yStart", yVal),
+                        yEnd: PlottableValue.value("yEnd", yVal + squareSize)
+                    )
+                    .foregroundStyle(by: .value("Count", statistic.female))
+                }
+            }
         }
+        .chartForegroundStyleScale(
+            range: Gradient(colors: Self.gradientColors)
+        )
+        .chartYAxis {
+            AxisMarks(position: .leading)
+        }
+        .frame(height: 500)
     }
+
+    // MARK: - Methods
 }
 
 struct HeatMapDemo_Previews: PreviewProvider {
